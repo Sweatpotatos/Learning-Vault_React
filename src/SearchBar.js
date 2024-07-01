892e81c05f5096c063f53ca6645e071abb386199
Mandy Jiang <137333628+mxndyj@users.noreply.github.com>
2024-07-01 15:37:50 -0700

    edit

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -1,15 +1,26 @@
-import React from 'react';
+import * as React from "react";
 import './SearchBar.css';
 
 function SearchBar() {
   return (
-    <div className="group">
-      <div className="SearchBar">
+    <div className="search-bar">
+      <div className="search-input">
+        <img
+          loading="lazy"
+          src="https://cdn.builder.io/api/v1/image/assets/TEMP/b889c61f38ba226f100bc50bf64d6cc35c5d8f8049c6b489edc99345d19b441e?"
+          className="search-icon"
+          alt="Search Icon"
+        />
         <input type="text" placeholder="Search for an Activity" />
-        <button type="submit" className="button">
-          <i className="fa fa-search icon"></i>
-        </button>
       </div>
+      <button className="search-button">
+        <img
+          loading="lazy"
+          src="https://cdn.builder.io/api/v1/image/assets/TEMP/0c79778c4c764cdd66754c3473fc37a4b64844240af6106123cf464369b1098c?"
+          className="button-icon"
+          alt="Search Button"
+        />
+      </button>
     </div>
   );
 }
