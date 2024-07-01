37a4478d64f46302202f2521cb5f4adc6cf3aef1
Mandy Jiang <137333628+mxndyj@users.noreply.github.com>
2024-07-01 23:00:24 +0000

    edit

--- src/Header.js
+++ src/Header.js
@@ -1,27 +1,21 @@
-import React from 'react';
+import React from "react";
 import './Header.css';
 
-function Header() {
+const Header = () => {
   return (
-    <header className="header">
-      <div className="header-logo">Learning Vault</div>
+    <header className="header-container">
+      <div className="header-logo">
+        <span className="font-bold">Learning</span> Vault
+      </div>
       <nav className="header-menu">
         <div className="header-menu-item selected">
-          Home
-          <div className="indicator"></div>
+          <div className="menu-text">Home</div>
+          <div className="indicator" />
         </div>
-        <div className="header-menu-item">
-          Activity List
-        </div>
-        <div className="header-menu-item">
-          New Arrivals
-        </div>
-        <div className="header-menu-item">
-          Gift Set
-        </div>
-        <div className="header-menu-item">
-          Visit Us
-        </div>
+        <div className="header-menu-item">Activity List</div>
+        <div className="header-menu-item">New Arrivals</div>
+        <div className="header-menu-item">Gift Set</div>
+        <div className="header-menu-item">Visit Us</div>
       </nav>
     </header>
   );
